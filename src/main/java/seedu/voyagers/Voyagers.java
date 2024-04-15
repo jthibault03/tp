e4c9a6aaac6af33959e3ea637ff86c0920f8a5d1
jthibault03 <98941496+jthibault03@users.noreply.github.com>
2024-04-15 13:24:45 +0800

    style check

--- src/main/java/seedu/voyagers/Voyagers.java
+++ src/main/java/seedu/voyagers/Voyagers.java
@@ -12,7 +12,7 @@ import seedu.voyagers.commands.ListCommand;
 
 import java.util.ArrayList;
 
-import static seedu.voyagers.utils.BillStorage.readBillFile;
+//import static seedu.voyagers.utils.BillStorage.readBillFile;
 //import static seedu.voyagers.utils.ProfileStorage.readProfileFile;
 import static seedu.voyagers.utils.ReviewStorage.readReviewFile;
 import static seedu.voyagers.utils.ReviewStorage.writeReviewFileMain;
